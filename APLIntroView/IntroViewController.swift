bc915badf482ac8d06ad3e7f63a7968f39077a92
Famara Kassama <>
2019-04-12 18:54:41 +0200

    declare functions as open and APLVideoPlayerView dependency as final for inheritance

--- APLIntroView/IntroViewController.swift
+++ APLIntroView/IntroViewController.swift
@@ -9,66 +9,65 @@
 import UIKit
 import APLVideoPlayerView
 
-protocol IntroViewControllerDelegate: NSObjectProtocol {
+public protocol IntroViewControllerDelegate: NSObjectProtocol {
     func introViewControllerDidFinishPlayingVideo(_ controller: IntroViewController)
     func introViewControllerDidGetTapped(_ controller: IntroViewController)
     func introViewControllerVideoPlayerIsReadyToPlay(_ controller: IntroViewController)
 }
 
-class IntroViewController: UIViewController {
-
+open class IntroViewController: UIViewController {
+    
     // MARK: public properties
-    weak var delegate: IntroViewControllerDelegate?
-    var imageName: String?
-    var videoName: String?
-    var canDismissViewControllerWithTouch = false
-    var videoDoesLoop = false
-    var isStatusBarHidden = false {
+    open weak var delegate: IntroViewControllerDelegate?
+    open var imageName: String?
+    open var videoName: String?
+    open var canDismissViewControllerWithTouch = false
+    open var videoDoesLoop = false
+    open var isStatusBarHidden = false {
         didSet {
             setNeedsStatusBarAppearanceUpdate()
         }
     }
-
+    
     // MARK: private properties
     private var imageView: UIImageView!
-    private var videoPlayerView: APLVideoPlayerView!
+    final private var videoPlayerView: APLVideoPlayerView!
     private static var keyValueObservationContext = 0
-
-    override var prefersStatusBarHidden: Bool {
+    
+    override open var prefersStatusBarHidden: Bool {
         return isStatusBarHidden
     }
-
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
         videoPlayerView.avPlayer.removeObserver(self, forKeyPath: "status", context: &IntroViewController.keyValueObservationContext)
     }
-
-    override func viewDidLoad() {
+    
+    override open func viewDidLoad() {
         super.viewDidLoad()
-
+        
         addImageView()
         addVideoPlayerView()
-
+        
         if canDismissViewControllerWithTouch {
             addTapGestureRecognizer()
         }
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(videoDidFinishNotification(notification:)), name: Notification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
-
     }
-
-    override func viewDidAppear(_ animated: Bool) {
+    
+    override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupVideoPlayer()
     }
-
-    func addImageView() {
+    
+    open func addImageView() {
         if let imageName = imageName {
             let image = UIImage(named: imageName)
             imageView = UIImageView(image: image)
             imageView.contentMode = .scaleAspectFill
             view.insertSubview(imageView, at: 0)
-
+            
             // auto layout constraints
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
@@ -77,11 +76,11 @@ class IntroViewController: UIViewController {
             imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         }
     }
-
-    func addVideoPlayerView() {
+    
+    open func addVideoPlayerView() {
         videoPlayerView = APLVideoPlayerView()
         view.insertSubview(videoPlayerView, at: 1)
-
+        
         // auto layout constraints
         videoPlayerView.translatesAutoresizingMaskIntoConstraints = false
         videoPlayerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
@@ -89,13 +88,13 @@ class IntroViewController: UIViewController {
         videoPlayerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         videoPlayerView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
-
-    func addTapGestureRecognizer() {
+    
+    public func addTapGestureRecognizer() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTouchUpInsideScreen(sender:)))
         view.addGestureRecognizer(tapGestureRecognizer)
     }
-
-    func setupVideoPlayer() {
+    
+    open func setupVideoPlayer() {
         if let videoFileName = videoName {
             videoPlayerView.setVideoFilename(videoFileName, loop: videoDoesLoop)
             videoPlayerView.videoGravity = .resizeAspectFill
@@ -104,21 +103,21 @@ class IntroViewController: UIViewController {
             videoPlayerView.play()
         }
     }
-
-    @objc func didTouchUpInsideScreen(sender: Any?) {
+    
+    @objc private func didTouchUpInsideScreen(sender: Any?) {
         delegate?.introViewControllerDidGetTapped(self)
     }
-
-    @objc func videoDidFinishNotification(notification: Notification) {
+    
+    @objc private func videoDidFinishNotification(notification: Notification) {
         delegate?.introViewControllerDidFinishPlayingVideo(self)
     }
-
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    
+    override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         guard context == &IntroViewController.keyValueObservationContext else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
             return
         }
-
+        
         if keyPath == "status" {
             if videoPlayerView.avPlayer.status == .readyToPlay {
                 delegate?.introViewControllerVideoPlayerIsReadyToPlay(self)
