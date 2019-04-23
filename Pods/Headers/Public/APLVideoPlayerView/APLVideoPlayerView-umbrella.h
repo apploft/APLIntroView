cda9aff5055783c1226d6dbbecb768f30b7f4ed6
Famara Kassama <>
2019-04-23 14:54:31 +0200

    pod update

--- Pods/Headers/Public/APLVideoPlayerView/APLVideoPlayerView-umbrella.h
+++ Pods/Headers/Public/APLVideoPlayerView/APLVideoPlayerView-umbrella.h
@@ -1,17 +1,1 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
-
-#import "APLVideoPlayerView.h"
-
-FOUNDATION_EXPORT double APLVideoPlayerViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char APLVideoPlayerViewVersionString[];
-
+../../../Target Support Files/APLVideoPlayerView/APLVideoPlayerView-umbrella.h
